8cdccb9f55f93d4b520c6607ce182315fb3e6d1e
mjstanleyjr <mjstanleyjr@gmail.com>
2018-12-02 12:41:23 -0800

    Try making it all a floor

--- game.cpp
+++ game.cpp
@@ -15,27 +15,19 @@ Game::Game()
 	{
 		for(int j = 0; j < 7; j++)
 		{
-			brewery[i][j] = new Space();
+			brewery[i][j] = new Floor();
 		}
 	}
 
-	//Make it all a Floor
-	for(int i = 1; i < 9; i++)
-	{
-		for(int j = 1; j < 6; j++)
-		{
-			delete brewery[i][j];
-			brewery[i][j] = NULL;
-			brewery[i][j] = new Floor(i, j);
-		}
-	}
+
+
 
 	for(int c = 0; c < 7; c++)
 	{
 		delete brewery[0][c];
 		brewery[0][c] = NULL;
 		brewery[0][c] = new Wall(0, c);
-		(brewery[0][c])->makeCeiling();
+		brewery[0][c]->makeCeiling();
 	}
 }
 
