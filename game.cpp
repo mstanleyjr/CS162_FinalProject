c2768f0f3b52f1d88093e0ab511de2b5f0513ac3
mjstanleyjr <mjstanleyjr@gmail.com>
2018-12-02 12:18:41 -0800

    Floor test

--- game.cpp
+++ game.cpp
@@ -19,7 +19,16 @@ Game::Game()
 		}
 	}
 
-
+	//Make it all a Floor
+	for(int i = 1; i < 9; i++)
+	{
+		for(int j = 1; j < 6; j++)
+		{
+			delete brewery[i][j];
+			brewery[i][j] = NULL;
+			brewery[i][j] = new Floor(i, j);
+		}
+	}
 }
 
 Game::~Game()
